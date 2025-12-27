40ead594f52dfee1b90a905798e97ac43f825966
Skiltra <101026764+Skiltra@users.noreply.github.com>
2025-12-27 17:24:06 +0000

    [v0.2.3.2]
    - some note changes, renames, and class variable define for state change

--- Scripts/npc.py
+++ Scripts/npc.py
@@ -1,5 +1,5 @@
 #########################################// Testing \\###############################################
-import random #TBD; used for some behaviours 
+import random #TBD; used for some behaviours, FIX: maybe could be rollSystem
 from events import rollSystem
 from player import inventory
 
@@ -8,13 +8,16 @@ source = None
 
 # TODO: use npc.json
 class behaviour(rollSystem):
+    stateFocus = "live" # Handles what is available by instances and stops processes if needed
+    
     def __init__(self, type, health=100):
         self.name = type
         self.health = health
         self.strength = 0
         self.alive = True
-        self.state = "passive" #primary condition for behavior
-        self.flags = 0 # trying to determine behaviour
+        
+        self.stateNPC = "passive" #primary condition for behavior
+        self.flags = 0 # tbehavioru switching TODO: make max 100.0
 
     def determineAI(self):
         if self.alive == "dead":
@@ -24,29 +27,29 @@ class behaviour(rollSystem):
         value = random.randint(1,5)
         if value > 2:
             self.flags + 1 # TODO: make relative addition instead of static
-
+#  TODO: do somethign with this
 class companion(behaviour, inventory):
     def __init__(self):
         behaviour.__init__(self)
         inventory.__init__(self)
-
+# TODO: do later
 class persistantNPC(behaviour, inventory):
     def __init__(self):
         behaviour.__init__(self)
         inventory.__init__(self)
         self.relation
         self.prefernces = {"talk": 0, "trade": 0}
-
+# TODO: delay do later
 class enemy(behaviour):
     def __init__(self):
         behaviour.__init__(self)
         self.loot = None
-
+##### I DONT KNOW WHAT TO DO WITH THIS; why it needs to be a seperate class
 class relations:
     def __init__(self):
         print("this relation")
 
-######### INITIALIZE
+######### INITIALIZE #########
 if npcLoad:
     npcLoad = False
     rel = relations()
